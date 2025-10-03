7c4b1803b0e3a04a5b15519e8714bd156925b696
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-03 12:39:24 +0530

    update in redux-intro: here we connection component to redux as legacy way means old way

--- 14-redux-intro/src/feature/accounts/BalanceDisplay.js
+++ 14-redux-intro/src/feature/accounts/BalanceDisplay.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { connect, useSelector } from "react-redux";
 
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
@@ -7,9 +7,27 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  const account = useSelector((store) => store.account);
-  return <div className="balance">{formatCurrency(account.balance)}</div>;
-}
+// mordern way to connect react to redux and get data/state form redux reducer
+//function BalanceDisplay() {
 
-export default BalanceDisplay;
+//const account = useSelector((store) => store.account);
+
+//const { balance: currencyBalance } = useSelector((store) => store.account);
+
+//return <div className="balance">{formatCurrency(currencyBalance)}</div>;
+
+//}
+//export default BalanceDisplay;
+
+//---------------------------------------------------------------------------------------------------
+
+// we are using old way for connect react to redux  before useConnect hook
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
+}
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+export default connect(mapStateToProps)(BalanceDisplay);
